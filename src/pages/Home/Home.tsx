4608c6481117899cf058423af58367a5fd43cfa8
unknown <oso.lindo@gmail.com>
2021-10-24 11:42:54 +0300

    added fetch function+films state+components injection

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,39 @@
+import { FilmCard } from "../../components/FilmCard/FilmCard"
+import { Toc } from "../../components/Toc/Toc"
+import { useEffect, useState } from "react"
+import { getFilms } from "../../services/services"
+import { Header } from "../../components/Header/Header"
+import { FilmDetails } from "../../interfaces/interfaces"
 import './Home.css'
 const Home = () => {
 
-    return (
-        <div>
-        </div>)
-}
+    const [filmsArray, setFilmsArray] = useState<FilmDetails[]>([])
 
+    const fetchFilms = async () => {
+        const films = await getFilms();
+        setFilmsArray([...films])
+    }
+
+    useEffect(() => {
+        fetchFilms()
+    }, [])
+
+
+    if (filmsArray.length > 0) {
+        return (
+            <div> 
+                <Header></Header>
+                <div className="main-container">
+                    <Toc></Toc>
+                    <FilmCard></FilmCard>
+                </div>
+            </div>
+        )
+    }
+
+    else {
+        return (null)
+    }
+}
 
 export { Home }
