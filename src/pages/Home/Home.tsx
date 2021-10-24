be21e996b2a36458ff611d3786b198fb63230412
unknown <oso.lindo@gmail.com>
2021-10-24 15:20:31 +0300

    added flexbox + icons + opening paragraph + styles

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,11 +3,19 @@ import { Toc } from "../../components/Toc/Toc"
 import { useEffect, useState } from "react"
 import { getFilms } from "../../services/services"
 import { Header } from "../../components/Header/Header"
-import { FilmDetails } from "../../interfaces/interfaces"
+import { faJedi } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import './Home.css'
 const Home = () => {
 
-    const [filmsArray, setFilmsArray] = useState<FilmDetails[]>([])
+    interface Film {
+        title: string
+        opening_crawl: string
+        episode_id: number
+    }
+
+    const [filmsArray, setFilmsArray] = useState<Film[]>([])
 
     const fetchFilms = async () => {
         const films = await getFilms();
@@ -18,11 +26,15 @@ const Home = () => {
         fetchFilms()
     }, [])
 
-
     if (filmsArray.length > 0) {
         return (
             <div>
                 <Header></Header>
+                <div className='openning-paragraph'>
+                    <FontAwesomeIcon icon={faJedi} className='jedi-icon' />
+                    <h1>Choose your favorite movies</h1>
+                    <FontAwesomeIcon icon={faJedi} className='jedi-icon' />
+                </div>
                 <div className="main-container">
                     <Toc filmsInfo={filmsArray} ></Toc>
                     <FilmCard filmsInfo={filmsArray}></FilmCard>
