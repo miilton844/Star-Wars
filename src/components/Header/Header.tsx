6b33d588041ddd81545946bfcbf96f1218332d04
unknown <oso.lindo@gmail.com>
2021-10-24 12:07:17 +0300

    add header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,20 @@
+import { Navbar } from "react-bootstrap"
+import { Container } from "react-bootstrap"
 import './Header.css'
 
 
 const Header = () => {
+  const logo =  process.env.PUBLIC_URL + "/star-wars-logo.png"
+
   return (
     <div>
+      <Navbar bg="dark" variant="dark" className='navbar'>
+        <Container  >
+          <Navbar.Brand>
+            <img id='logo' src= {logo}></img>
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
     </div>
   )
 
