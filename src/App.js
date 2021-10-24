3722434359d8ef8108e8235e12d7d2e2fd484043
unknown <oso.lindo@gmail.com>
2021-10-24 10:50:53 +0300

    update App - added home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { Home } from './pages/Home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 function App() {
   return (
     <div className="App">
-    Hello
+    <Home></Home>
     </div>
   );
 }
